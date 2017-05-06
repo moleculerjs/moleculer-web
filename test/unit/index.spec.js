4f3f11b720c757ff3f88aee5e60942edb21a0ed9
Icebob <mereg.norbert@gmail.com>
2017-05-06 23:29:49 +0200

    add test spec skeleton

---
+++ test/unit/index.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const ApiGateway = require("../../src");
+const { ServiceBroker } = require("Moleculer");
+
+
+describe("Test constructor", () => {
+
+	it("should do something", () => {
+		
+	});
+});
