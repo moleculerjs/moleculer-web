0692668eefabdfa8f5d877668c4fc1c43a26ae09
Icebob <mereg.norbert@gmail.com>
2017-10-28 14:58:44 +0200

    test middlewares

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1507,6 +1507,82 @@ describe("Test onBeforeCall & onAfterCall", () => {
 
 });
 
+describe("Test route middlewares", () => {
+
+	it("should call both middlewares", () => {
+		const broker = new ServiceBroker();
+		broker.loadService("./test/services/test.service");
+
+		const mw1 = jest.fn((req, res, next) => {
+			res.setHeader("X-Custom-Header", "middleware");
+			next();
+		});
+
+		const mw2 = jest.fn((req, res, next) => {
+			next();
+		});
+
+		const service = broker.createService(ApiGateway, {
+			settings: {
+				routes: [{
+					path: "/",
+					use: [mw1, mw2]
+				}]
+			}
+		});
+		const server = service.server;
+
+		return request(server)
+			.get("/test/hello/")
+			.expect(200)
+			.expect("Content-Type", "application/json")
+			.expect("X-Custom-Header", "middleware")
+			.then(res => {
+				expect(res.body).toBe("Hello Moleculer");
+				expect(mw1).toHaveBeenCalledTimes(1);
+				expect(mw1).toHaveBeenCalledWith(jasmine.any(http.IncomingMessage), jasmine.any(http.ServerResponse), jasmine.any(Function));
+
+				expect(mw2).toHaveBeenCalledTimes(1);
+				expect(mw2).toHaveBeenCalledWith(jasmine.any(http.IncomingMessage), jasmine.any(http.ServerResponse), jasmine.any(Function));
+			});
+	});
+
+	it("should return with error if middlewares call next with error", () => {
+		const broker = new ServiceBroker();
+		broker.loadService("./test/services/test.service");
+
+		const mw1 = jest.fn((req, res, next) => {
+			next(new Error("Something went wrong"));
+		});
+
+		const mw2 = jest.fn((req, res, next) => {
+			next();
+		});
+
+		const service = broker.createService(ApiGateway, {
+			settings: {
+				routes: [{
+					path: "/",
+					use: [mw1, mw2]
+				}]
+			}
+		});
+		const server = service.server;
+
+		return request(server)
+			.get("/test/hello")
+			.expect(500)
+			.then(res => {
+				expect(res.text).toBe("Server error! Something went wrong");
+				expect(mw1).toHaveBeenCalledTimes(1);
+				expect(mw1).toHaveBeenCalledWith(jasmine.any(http.IncomingMessage), jasmine.any(http.ServerResponse), jasmine.any(Function));
+
+				expect(mw2).toHaveBeenCalledTimes(0);
+			});
+	});
+
+});
+
 describe("Test authorization", () => {
 
 	it("don't enabled authorization if missing 'authorize' method", () => {
