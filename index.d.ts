56ffc0062a7a93813471e86be9850a311db9a9e0
David Vallner <david@vallner.net>
2018-04-14 16:02:17 +0200

    fix type definition of Errors

--- index.d.ts
+++ index.d.ts
@@ -2,18 +2,18 @@ declare module "moleculer-web" {
     import { Errors, ServiceSchema } from "moleculer";
     class InvalidRequestBodyError extends Errors.MoleculerError { constructor(body: any, error: any) }
     class InvalidResponseTypeError extends Errors.MoleculerError { constructor(dataType: string) }
-    class UnAuthorizedError extends Errors.MoleculerError { constructor(type: string, data: any) }
+    class UnAuthorizedError extends Errors.MoleculerError { constructor(type: string|null|undefined, data: any) }
     class ForbiddenError extends Errors.MoleculerError { constructor(type: string, data: any) }
     class BadRequestError extends Errors.MoleculerError { constructor(type: string, data: any) }
     class RateLimitExceeded extends Errors.MoleculerClientError { constructor(type: string, data: any) }
 
     interface ApiGatewayErrors {
-        InvalidRequestBodyError: InvalidRequestBodyError;
-        InvalidResponseTypeError: InvalidResponseTypeError;
-        UnAuthorizedError: UnAuthorizedError;
-        ForbiddenError: ForbiddenError;
-        BadRequestError: BadRequestError;
-        RateLimitExceeded: RateLimitExceeded;
+        InvalidRequestBodyError: typeof InvalidRequestBodyError;
+        InvalidResponseTypeError: typeof InvalidResponseTypeError;
+        UnAuthorizedError: typeof UnAuthorizedError;
+        ForbiddenError: typeof ForbiddenError;
+        BadRequestError: typeof BadRequestError;
+        RateLimitExceeded: typeof RateLimitExceeded;
 
         ERR_NO_TOKEN: "ERR_NO_TOKEN";
         ERR_INVALID_TOKEN: "ERR_INVALID_TOKEN";
