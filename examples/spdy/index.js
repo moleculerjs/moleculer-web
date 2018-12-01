1d7340bfa4ccbcea3ffc4fd20ee82db0f5db97ba
Icebob <mereg.norbert@gmail.com>
2018-12-01 19:54:44 +0100

    simpler spdy example

--- examples/spdy/index.js
+++ examples/spdy/index.js
@@ -32,7 +32,20 @@ broker.createService({
 	mixins: ApiGatewayService,
 
 	settings: {
-		server: false,
+		key: fs.readFileSync(path.join(__dirname, "../ssl/key.pem")),
+		cert: fs.readFileSync(path.join(__dirname, "../ssl/cert.pem")),
+
+		// **optional** SPDY-specific options
+		spdy: {
+			protocols: [ "h2", "spdy/3.1", "http/1.1" ],
+			plain: false,
+			"x-forwarded-for": true,
+			connection: {
+				windowSize: 1024 * 1024, // Server's window size
+				autoSpdy31: false
+			}
+		},
+
 		routes: [{
 			whitelist: [
 				"test.hello",
@@ -44,48 +57,12 @@ broker.createService({
 		}]
 	},
 
-	created() {
-		// More info: https://github.com/spdy-http2/node-spdy#usage
-		this.server = spdy.createServer({
-			key: fs.readFileSync(path.join(__dirname, "../ssl/key.pem")),
-			cert: fs.readFileSync(path.join(__dirname, "../ssl/cert.pem")),
-
-			// **optional** SPDY-specific options
-			spdy: {
-				protocols: [ "h2", "spdy/3.1", "http/1.1" ],
-				plain: false,
-				"x-forwarded-for": true,
-				connection: {
-					windowSize: 1024 * 1024, // Server's window size
-					autoSpdy31: false
-				}
-			}
-		}, this.httpHandler);
-	},
-
-	started() {
-		return new this.Promise((resolve, reject) => {
-			this.server.listen(this.settings.port, this.settings.ip, err => {
-				if (err)
-					return reject(err);
-
-				this.logger.info("SPDY server started.");
-				this.logger.info(chalk.yellow.bold(`Open https://localhost:${this.settings.port}/test/hello in your browser.`));
-				resolve();
-			});
-		});
-	},
-
-	stopped() {
-		return new this.Promise((resolve, reject) => {
-			this.server.close(err => {
-				if (err)
-					return reject(err);
-
-				this.logger.info("SPDY server stopped.");
-				resolve();
-			});
-		});
+	methods: {
+		createServer() {
+			// More info: https://github.com/spdy-http2/node-spdy#usage
+			this.server = spdy.createServer(this.settings, this.httpHandler);
+			this.isHTTPS = true;
+		}
 	}
 });
 
