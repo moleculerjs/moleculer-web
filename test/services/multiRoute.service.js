ebfe3e0a1f38a972ce77deb18e08cf8ae780e570
mariusbackes <mariusbackes@icloud.com>
2020-11-20 13:53:57 +0100

    added multiple REST routes in service settings

---
+++ test/services/multiRoute.service.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const _ = require("lodash");
+
+const { MoleculerServerError } = require("moleculer").Errors;
+
+module.exports = {
+    name: "multiRoute",
+
+    settings: {
+        rest: ["/route", "/route/multi"]
+    },
+
+    actions: {
+        hello: {
+            rest: "GET /hello",
+            handler(ctx) {
+                return "Hello Moleculer";
+            }
+        },
+
+        greet: {
+            rest: "/greet",
+            params: {
+                name: "string"
+            },
+            handler(ctx) {
+                return `Hello ${ctx.params.name}`;
+            }
+        },
+
+        fullPath: {
+            rest: {
+                method: "GET",
+                fullPath: "/fullPath",
+            },
+            handler(ctx) {
+                return "Full path";
+            }
+        },
+
+        basePath: {
+            rest: {
+                method: "GET",
+                path: "/base-path",
+                basePath: "custom-base-path"
+            },
+            handler(ctx) {
+                return "Hello Custom Moleculer Root Path";
+            }
+        },
+    }
+}
